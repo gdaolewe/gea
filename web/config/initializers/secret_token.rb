ce08c330f9262dce0de5c1ffa94e74e295f8476f
Kenneth Powers <mail@kenpowers.net>
2013-03-01 16:09:02 -0500

    Generated initial RoR application.

---
+++ web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = 'cd2405f0ea063ba8f00b3623f911afb132b7aa6ddfd184b6df8fa294d13f40f2e4303b801d3e67b6e543daca8d78d452e54d866fd3c4ffaa92095479108d8881'
