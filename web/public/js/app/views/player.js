5b06ea7b6e3dec7335d532bb8a3cb28cdb1260bc
cheni <pheonix7117@gmail.com>
2013-03-28 21:25:41 -0400

    Rdio playback works with album art and track metadata. Play/pause and prev/next buttons are wired.

--- web/public/js/app/views/player.js
+++ web/public/js/app/views/player.js
@@ -21,25 +21,35 @@ define([
     },
     initialize: function () {
       this.$el;//jquery object of the div
+      this.$duration = 1;
       this.$api = this.$('#api');
+      // Start playing the album with id a171827 when ready
       this.$api.bind('ready.rdio', $.proxy(function() {
         this.$api.rdio().play('a171827');
       }, this));
       
+      // When the playing track has changed, adjust the album art, track, album, and artist info accordingly
+      this.$api.bind('playingTrackChanged.rdio', function(e, playingTrack, sourcePosition) {
+        if (playingTrack) {
+          this.$duration = playingTrack.duration;
+          $('#player-art').attr('src', playingTrack.icon);
+          $('#track').text(playingTrack.name);
+          $('#artist').text('by ' + playingTrack.artist);
+          $('#album').text('from ' + playingTrack.album);
+        }
+      });
       $.get('/rdio/getPlaybackToken', $.proxy(function (data) {
         this.$api.rdio(data.result);
       }, this));
       this.$playPauseButton = this.$('#play-pause');
       this.$playPauseButton.text(playingText[playing]);
       this.$progressBarFill = this.$('#fill');
-      this.$metadata = this.$('#player-metadata');
-      this.$('#player-art').text('Art loaded.');
     },
     togglePlay: function (e) {
       e.stopPropagation();
       e.preventDefault();
-      this.$api.rdio().play();
       this.$playPauseButton.text(playingText[++playing % 2]);
+      playing % 2 === 0 ? this.$api.rdio().pause() : this.$api.rdio().play();
       this.$progressBarFill.css('-webkit-animation-play-state', function (i, v) {
         return v === 'paused' ? 'running' : 'paused';
       });
@@ -48,10 +58,9 @@ define([
       e.stopPropagation();
       e.preventDefault();
       this.$api.rdio().next();
-      var songId = song.id;
-      if (++songId >= 5) {
-        songId = 0;
-      }
+      playing = 1;
+      this.$playPauseButton.text(playingText[playing % 2]);
+      this.$progressBarFill.css('-webkit-animation-play-state', 'running');
       /*
       Song.get(songId, $.proxy(function (s) {
         song = s;
@@ -62,10 +71,10 @@ define([
     playPrevious: function (e) {
       e.stopPropagation();
       e.preventDefault();
-      var songId = song.id;
-      if (--songId <= 0) {
-        songId = 4;
-      }
+      this.$api.rdio().previous();
+      playing = 1;
+      this.$playPauseButton.text(playingText[playing % 2]);
+      this.$progressBarFill.css('-webkit-animation-play-state', 'running');
       /*
       Song.get(songId, $.proxy(function (s) {
         song = s;
