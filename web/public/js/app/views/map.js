c155520c7040e78b0e0a5f88bb6fcf2511cf6c06
Kim Cho <kcho47@gmail.com>
2013-03-31 12:25:13 -0400

    Map added. Currently fills entire page width.

--- web/public/js/app/views/map.js
+++ web/public/js/app/views/map.js
@@ -9,6 +9,13 @@ define([
     el: '#map',
     initialize: function () {
       this.$el.text('Map loaded.');
+      this.mapOptions = {
+          center: new google.maps.LatLng(42.375200, -72.521200),
+          zoom: 10,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        this.map = new google.maps.Map(this.el,this.mapOptions);
+        //google.maps.event.addDomListener(window, 'load', initialize);
     }
   }))();
 });
