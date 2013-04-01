342707d25eda1d1b2ede8ac0ff1373498f975634
Kenneth Powers <mail@kenpowers.net>
2013-03-31 20:29:10 -0400

    Fixed formatting.

--- web/public/js/app/views/map.js
+++ web/public/js/app/views/map.js
@@ -8,13 +8,13 @@ define([
   return new (bb.View.extend({
     el: '#map',
     initialize: function () {
-      this.$el.text('Map loaded.');
       this.mapOptions = {
-          center: new google.maps.LatLng(42.375200, -72.521200),
-          zoom: 10,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        this.map = new google.maps.Map(this.el,this.mapOptions);
+        center: new google.maps.LatLng(42.375200, -72.521200),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      this.map = new google.maps.Map(this.el, this.mapOptions);
+      this.resize();
     }
   }))();
 });
