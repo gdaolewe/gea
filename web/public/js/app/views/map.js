1fc3f248fbdb7f77195211e0ae7aae7239272da7
Kim Cho <kcho47@gmail.com>
2013-04-19 18:28:44 -0400

    Implemented pins in preparation for Spiderfier

--- web/public/js/app/views/map.js
+++ web/public/js/app/views/map.js
@@ -5,15 +5,39 @@ define([
   'util/jqr!'
 ], function (bb) {
   // Maps API available as the variable `google`
+  var marker = null;
   return new (bb.View.extend({
     el: '#map',
     initialize: function () {
+      //both pins and mapCenter are currently set to Amherst, MA
+      this.markerLocation = new google.maps.LatLng(42.375200, -72.521200);
+      this.mapCenter = new google.maps.LatLng(42.375200, -72.521200);
       this.mapOptions = {
-        center: new google.maps.LatLng(42.375200, -72.521200),
+        center: this.mapCenter,
         zoom: 10,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
       this.map = new google.maps.Map(this.el, this.mapOptions);
+      setTimeout($.proxy(function () {
+        this.addAllMarkers();
+      }, this), 200);
+    },
+
+    addNewMarker: function () {
+      marker = new google.maps.Marker({
+        position: this.markerLocation,
+        map: this.map,
+        title:"Hello World!",
+        animation: google.maps.Animation.DROP
+      });
+    },
+
+    addAllMarkers: function () {
+      for( var i = 0; i <= 10; i++){
+        setTimeout($.proxy(function () {
+          this.addNewMarker();
+        }, this), i*200);
+      }
     }
   }))();
 });
