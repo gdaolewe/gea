9673c11bb9d21be6f425f0fac23653979c2632de
cheni <pheonix7117@gmail.com>
2013-04-25 19:38:39 -0400

    Joyride (tour) working! (with cookies omnomnom)

--- web/public/js/app/views/tour.js
+++ web/public/js/app/views/tour.js
@@ -8,7 +8,6 @@ define([
     el: '#joyRideTipContent',
     initialize: function () {
       this.$el.joyride({
-        'startOffset':2,
         'tipLocation': 'bottom',         // 'top' or 'bottom' in relation to parent
         'nubPosition': 'auto',           // override on a per tooltip bases
         'scrollSpeed': 800,              // Page scrolling speed in ms
@@ -16,15 +15,11 @@ define([
         'startTimerOnClick': false,       // true/false to start timer on first click
         'nextButton': true,              // true/false for next button visibility
         'tipAnimation': 'fade',           // 'pop' or 'fade' in each tip
-        'pauseAfter': [],                // array of indexes where to pause the tour after
         'tipAnimationFadeSpeed': 500,    // if 'fade'- speed in ms of transition
-        'cookieMonster': false,           // true/false for whether cookies are used
+        'cookieMonster': true,           // true/false for whether cookies are used
         'cookieName': 'JoyRide',         // choose your own cookie name
         'cookieDomain': false,           // set to false or yoursite.com
-        'tipContent': '#joyRideTipContent'
-        //'tipContainer': body,            // Where the tip be attached if not inline
-        //'postRideCallback': $noop,       // a method to call once the tour closes
-        //'postStepCallback': $noop        // A method to call after each step
+        'tipContent': '#joyRideTipContent' // the ol element that lists the tips
       });
       this.$el.joyride();
     }
