f5f1170b9233d0e62abfb5b71f3c8865f91f5c71
cheni <pheonix7117@gmail.com>
2013-04-25 21:10:03 -0400

    Checking cookie to see if user has already viewed the tour first. Also using cloudflare for jquery.cookie.

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -14,12 +14,15 @@ require({
       '//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.10/backbone-min',
       'lib/backbone.min'
     ],
+    'cookie': [
+      '//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.3.1/jquery.cookie.min',
+      'lib/jquery.cookie'
+    ],
     'jqueryrdio': 'lib/jquery.rdio.min',
     'promise': 'lib/promise',
     'async': 'lib/async',
     'text': 'lib/text',
-    'joyride': 'lib/jquery.joyride',
-    'cookie': 'lib/jquery.cookie'
+    'joyride': 'lib/jquery.joyride'
   },
   shim: {
     'backbone': {
