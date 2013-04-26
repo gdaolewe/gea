240d63e389c5510cef638f928d9f2267777dcbaf
Kim Cho <kcho47@gmail.com>
2013-04-25 22:53:13 -0400

    Added dummy data to InfoWindow for each pin. InfoWindow displays dummy song metadata and album art.

--- web/public/js/app/views/map.js
+++ web/public/js/app/views/map.js
@@ -5,7 +5,8 @@ define([
   'util/jqr!'
 ], function (bb) {
   // Maps API available as the variable `google`
-  var marker = null;
+  var contentMap = null;
+  var iw = null;
   return new (bb.View.extend({
     el: '#map',
     initialize: function () {
@@ -18,25 +19,47 @@ define([
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
       this.map = new google.maps.Map(this.el, this.mapOptions);
-      this.oms = new OverlappingMarkerSpiderfier(this.map);
-      /*sets up listeners
+      this.oms = new OverlappingMarkerSpiderfier(this.map, {keepSpiderfied: true});
       
-      var iw = new google.maps.InfoWindow();
-      this.oms.addListener('click', function(marker) {
-        iw.setContent(marker.desc);
-        iw.open(this.map, this.marker);
-      });*/
+// this is the code for grabbing the top 10 from the server
+// need to figure out how to get this information into the pin information
+// also want to figure out how to get album art into the pins to make custom ones
+// be adding the pins as we go with this information so won't need the marker functions anymore
+        /*$.get('/rate?limit=10', function (data) {
+        var alertText = '';
+        var counter = 1;
+        data.forEach($.proxy(function (d) {
+          var result = '';
+          if (d.title) result += '\'' + d.title + '\'';
+          if (d.artist) result += ' by ' + d.artist;
+          if (d.album) result += ' from \'' + d.album + '\'';
+          if (result) {
+            alertText += counter + '. ' + result + '\n';
+            counter++;
+          }
+        }, this));*/
       setTimeout($.proxy(function () {
         this.addAllMarkers();
       }, this), 200);
-    },
+            /*sets up listeners*/
+      iw = new google.maps.InfoWindow();
+      this.oms.addListener('click', $.proxy(function(m) {
+        iw.setContent(m.desc);
+        iw.open(this.map, m);
+      }, this));
 
+    },
+//maybe add listener to infowindow to listen for click on album art to play it
     addNewMarker: function () {
-      marker = new google.maps.Marker({
+      var marker = new google.maps.Marker({
         position: this.markerLocation,
         map: this.map,
         title:"Hello World!",
-        animation: google.maps.Animation.DROP
+        //animation: google.maps.Animation.DROP, //removed for easier testing
+        desc: '<span id="info-window"><img src="http://cdn3.rd.io/album/3/3/f/0000000000029f33/square-200.jpg" width=100 height=100>'+
+                '<span><div id="iw-title">Song: Planet Telex</div>'+
+                '<div id="iw-album">Album: The Bends</div>'+
+                '<div id="iw-artist">Artist: Radiohead</div></span></span>'
       });
       this.oms.addMarker(marker);
     },
