7b449a77470a7892f953e7c77eb0d83b4073780e
Kenneth Powers <mail@kenpowers.net>
2013-04-27 01:33:44 -0400

    Fixed sql error: rdioId must be included in GROUP BY.

--- web/db/dml/getRatings.sql
+++ web/db/dml/getRatings.sql
@@ -1,6 +1,6 @@
 SELECT artist, album, title, "rdioId", SUM(rating) as score
 FROM songs, ratings
 WHERE songs.id = ratings.sid{EXTRA_WHERE}
-GROUP BY artist, album, title
+GROUP BY artist, album, title, "rdioId"
 ORDER BY score {ORDER}
 LIMIT {LIM} OFFSET {OFF}
