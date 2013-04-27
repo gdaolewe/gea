2237a293cc6491ff167a72954f605a9c65e7bd91
Kenneth Powers <mail@kenpowers.net>
2013-04-27 00:04:20 -0400

    Added rdioId to GET /rate.
    Closes #111

--- web/db/dml/getRatings.sql
+++ web/db/dml/getRatings.sql
@@ -1,4 +1,4 @@
-SELECT artist, album, title, SUM(rating) as score
+SELECT artist, album, title, "rdioId", SUM(rating) as score
 FROM songs, ratings
 WHERE songs.id = ratings.sid{EXTRA_WHERE}
 GROUP BY artist, album, title
