c71e69a03b2b9c089a189c3a03c39e62a40c2d27
cheni <pheonix7117@gmail.com>
2013-04-28 15:59:39 -0400

    Social buttons limited to one per track.

--- web/public/js/app/views/player.js
+++ web/public/js/app/views/player.js
@@ -20,7 +20,9 @@ define([
   var dragging = false;
   var deferred = new promise.Promise();
   var streamerPromise = new promise.Promise();
-  var currentStreamId = 'a171827';
+  var currentStreamId = null;
+  var likeEnabled = true;
+  var dislikeEnabled = true;
 
   return new (bb.View.extend({
     el: '#player',
@@ -50,7 +52,6 @@ define([
       // Queue the first album when ready
       streamerPromise.then($.proxy(function () {
         this.$streamer.bind('ready', $.proxy(function() {
-          this.$streamer.queue(currentStreamId);
           // Listen for new songs to play
           vent.on('play-key', $.proxy(function (key) {
             currentStreamId = key;
@@ -81,8 +82,12 @@ define([
         this.$streamer.bind('playingTrackChanged', $.proxy(function(e, playingTrack, sourcePosition) {
           deferred.done();
           if (playingTrack) {
+            //Get the current stream key
+            currentStreamId = playingTrack.key;
             //Store which track this is in the current playing source
             this.sourcePosition = sourcePosition;
+            //Ensure the like/dislike buttons are enabled
+            this.enableSocialButtons();
             //Set global boolean to indicate if this is the last track in the source or not
             lastTrack = (this.sourcePosition === this.sourceLength - 1);
             //If this is the last track, switch to the 'disabled' button
@@ -150,6 +155,7 @@ define([
       this.$progressBar = this.$('#progress-bar');
       this.$progressBarFill = this.$('#fill');
 	    this.$likeImg = this.$('#like'); //creates global var in initialized function
+      this.$dislikeImg = this.$('#dislike');
     },
     togglePlay: function (e) {
       e.stopPropagation();
@@ -170,7 +176,7 @@ define([
           console.log("last!");
           return;
         }
-        loading = true;
+        this.loading();
         this.$streamer.next();
       }, this));
     },
@@ -187,7 +193,7 @@ define([
         } else {
           //Otherwise, seek to the beginning of this track and then change to the previous (ensures the right behavior)
           this.$streamer.seek(0);
-          loading = true;
+          this.loading();
           this.$streamer.previous();
         }
       }, this));
@@ -195,16 +201,32 @@ define([
     like: function (e) {
       e.stopPropagation();
       e.preventDefault();
-      $.post('/rate?from=' + this.$streamer.name + '&id=' + currentStreamId + '&verdict=like', function () {
-        alert('Like submitted! :D');
-      });
+      if (!loading && currentStreamId && likeEnabled) {
+        $.post('/rate?from=' + this.$streamer.name + '&id=' + currentStreamId + '&verdict=like', $.proxy(function () {
+          this.$likeImg.toggleClass('disabled');
+          likeEnabled = false;
+          if (!dislikeEnabled) {
+            this.$dislikeImg.toggleClass('disabled');
+            dislikeEnabled = true;
+          }
+          alert('Like submitted! :D');
+        }, this));
+      }
     },
     dislike: function (e) {
       e.stopPropagation();
       e.preventDefault();
-      $.post('/rate?from=' + this.$streamer.name + '&id=' + currentStreamId + '&verdict=dislike', function () {
-        alert('Dislike submitted! >:(');
-      });
+      if (!loading && currentStreamId && dislikeEnabled) {
+        $.post('/rate?from=' + this.$streamer.name + '&id=' + currentStreamId + '&verdict=dislike', $.proxy(function () {
+          if (!likeEnabled) {
+            this.$likeImg.toggleClass('disabled');
+            likeEnabled = true;
+          }
+          this.$dislikeImg.toggleClass('disabled');
+          dislikeEnabled = false;
+          alert('Dislike submitted! >:(');
+        }, this));
+      }
     },
     drag: function (e) {
       e.stopPropagation();
@@ -248,6 +270,20 @@ define([
     },
     updatePlayPauseButton: function () {
       this.$playPauseButton.attr('class', playingClass[playing ? 1 : 0]);
+    },
+    enableSocialButtons: function () {
+      if (!likeEnabled) {
+        this.$likeImg.toggleClass('disabled');
+        likeEnabled = true;
+      }
+      if (!dislikeEnabled) {
+        this.$dislikeImg.toggleClass('disabled');
+        dislikeEnabled = true;
+      }
+    },
+    loading: function () {
+      loading = true;
+      currentStreamId = null;
     }
   }))();
 });
