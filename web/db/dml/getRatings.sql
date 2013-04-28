7df1862370ff9aeb6655f55c1f304a2482970b39
Kenneth Powers <mail@kenpowers.net>
2013-04-28 00:47:56 -0400

    Server now returns image with ratings.

--- web/db/dml/getRatings.sql
+++ web/db/dml/getRatings.sql
@@ -4,11 +4,11 @@
 --GROUP BY artist, album, title, "rdioId"
 --ORDER BY score {--ORDER--}
 --LIMIT {--LIM--} OFFSET {--OFF--}
-SELECT coords, artist, album, title, "rdioId", score FROM (
-  SELECT coords, artist, album, title, "rdioId", SUM(rating) as score, row_number() OVER (PARTITION BY locations.id) AS rank
+SELECT coords, artist, album, title, "rdioId", image, score FROM (
+  SELECT coords, artist, album, title, "rdioId", image, SUM(rating) as score, row_number() OVER (PARTITION BY locations.id) AS rank
   FROM songs, ratings, locations
   WHERE songs.id = ratings.sid AND ratings.lid = locations.id{EXTRA_WHERE}
-  GROUP BY locations.id, coords, artist, album, title, "rdioId"
+  GROUP BY locations.id, coords, artist, album, title, "rdioId", image
 ) scored_songs_by_lid
 WHERE rank <= {LIM}
 ORDER BY score DESC
