d23980f70e4e2f53ae7e5c106842b37f50c8ffe7
Kim Cho <kcho47@gmail.com>
2013-04-29 00:22:11 -0400

    Pins now change color on spiderfying.

--- web/public/js/app/views/map.js
+++ web/public/js/app/views/map.js
@@ -14,7 +14,6 @@ define([
   var contentMap = null;
   var iw = null;
   var markerArray = [];
-  //var newImage = http://maps.google.com/mapfiles/ms/icons/blue-dot.png;
 
   return new (bb.View.extend({
     el: '#map',
@@ -44,22 +43,55 @@ define([
         iw.open(this.map, m);
       }, this));
 
-      /*this.oms.addListener('spiderfy', $.proxy(function(markers) {
+      //creating the MarkerImage for the default
+      this.redIcon = {
+        url: '/img/sprite.png',
+        size: new google.maps.Size(20,34),
+        origin: new google.maps.Point(100,400), 
+        //for anchor, origin x+10, origin y+34
+        anchor: new google.maps.Point(10,34) 
+      };
+
+      this.redIconShadow = {
+        url: '/img/sprite.png',
+        // The shadow image is larger in the horizontal dimension
+        // while the position and offset are the same as for the main image.
+        size: new google.maps.Size(37, 34),
+        origin: new google.maps.Point(120,434),
+        anchor: new google.maps.Point(10, 34)
+      };
+
+      this.blueIcon = {
+        url: '/img/sprite.png',
+        size: new google.maps.Size(20,34),
+        origin: new google.maps.Point(100,434),
+        anchor: new google.maps.Point(10,34)
+      };
+
+      this.blueIconShadow = {
+        url: '/img/sprite.png',
+        // The shadow image is larger in the horizontal dimension
+        // while the position and offset are the same as for the main image.
+        size: new google.maps.Size(37, 34),
+        origin: new google.maps.Point(120,434),
+        anchor: new google.maps.Point(10, 34)
+      };
+
+      this.oms.addListener('spiderfy', $.proxy(function(markers) {
         for(var i = 0; i < markers.length; i ++) {
-          markers[i].setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png'); //do this in the template file?
-          //markers[i].setShadow(null);
+          markers[i].setIcon(this.blueIcon);
+          markers[i].setShadow(this.blueIconShadow);
         } 
         iw.close();
       }, this));
 
       this.oms.addListener('unspiderfy', $.proxy(function(markers) {
         for(var i = 0; i < markers.length; i ++) {
-          //need to set it to be the default icon again
-          //markers[i].setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png');
-          //markers[i].setShadow(null);
+          markers[i].setIcon(this.redIcon);
+          markers[i].setShadow(this.redIconShadow);
         } 
         iw.close();
-      }, this));*/
+      }, this)); 
 
       vent.on('mapFilter', $.proxy(function (hours) {
         this.clearMarkers();
@@ -67,28 +99,26 @@ define([
       }, this));
     },
 
-    /*TODO
-      different colors on spiderfying
-    */
-
     loadAllMarkers: function (hours) {
       var timePeriod = (hours) ? "&pastHours=" + hours : "";
       $.get('/rate?limit=10' + timePeriod, $.proxy(function (data) {
         for (var position in data) {
+          var rank = 1;
           data[position].reverse().forEach($.proxy(function (d) {
             var split = position.split(",");
             var latLng = new google.maps.LatLng(split[0], split[1]);
-            this.addNewMarker(latLng, d.title, d.artist, d.album, d.image, d.score, d.rdioId);
+            this.addNewMarker(latLng, d.title, d.artist, d.album, d.image, d.score, d.rdioId, rank);
+            rank++;
           }, this));
         }
       }, this));
     },
 
-    addNewMarker: function (position, song, artist, album, image, score, key) {
+    addNewMarker: function (position, song, artist, album, image, score, key, rank) {
       var marker = new google.maps.Marker({
         position: position,
         map: this.map,
-        title: song,
+        title: "#" + rank + ". " + song,
         desc: template({
           image: image,
           song: song,
@@ -121,6 +151,5 @@ define([
     playSong: function () {
       vent.trigger('play-key', this.$('#iw-key').val());
     }
-
   }))();
 });
