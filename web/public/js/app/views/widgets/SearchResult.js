735b43efde06cbc0c5909e734941809a2d108093
cheni <pheonix7117@gmail.com>
2013-04-28 23:42:05 -0400

    Fixed search results display.

--- web/public/js/app/views/widgets/SearchResult.js
+++ web/public/js/app/views/widgets/SearchResult.js
@@ -15,6 +15,9 @@ define([
   // Template
   var template = _.template(html);
 
+  //Truncating length
+  var maxLength = 25;
+
   // View constructor
   // These views represent a search result
   return bb.View.extend({
@@ -26,19 +29,24 @@ define([
       this.render();
     },
     render: function () {
+      var nameTruncated = this.model.get('name');
+      nameTruncated = (nameTruncated && nameTruncated.length > maxLength) ?
+                          $.trim(nameTruncated).substring(0, maxLength).trim(this) + "..."
+                          : nameTruncated;
+      var artistTruncated = this.model.get('artist');
+      artistTruncated = (artistTruncated && artistTruncated.length > maxLength) ?
+                          "by " + $.trim().substring(0, maxLength).trim(this) + "..."
+                          : (artistTruncated) ? "by " + artistTruncated : "";
       this.$el.html(template({
         icon: this.model.get('icon'),
-        artist: this.model.get('artist'),
+        artist: artistTruncated,
         key: this.model.get('key'),
-        name: this.model.get('name')
+        name: nameTruncated
       }));
 
       if (this.$el.hasClass('r')) {
         // Artist results don't have an artist field (ironically)
         this.$('.artist').remove();
-      } else if (this.$el.hasClass('a')) {
-        // Remove album from album results
-        this.$('.album').remove()
       }
     },
     click: function () {
