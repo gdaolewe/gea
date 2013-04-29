c70e85cf37a463c805ba44badd3eb2a42d960fb2
nh0815 <nh0815@gmail.com>
2013-04-29 02:43:10 -0400

    Tests implemented.

--- android/GeaTest/src/net/kenpowers/gea/test/RequestTaskTest.java
+++ android/GeaTest/src/net/kenpowers/gea/test/RequestTaskTest.java
@@ -1,6 +1,8 @@
 
 package net.kenpowers.gea.test;
 
+import static org.junit.Assert.*;
+
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,64 +13,66 @@ import org.junit.Before;
 
 import net.kenpowers.gea.GeaServerHandler;
 import net.kenpowers.gea.GeaServerHandler.RequestMethod;
-import android.test.AndroidTestCase;
 
-public class RequestTaskTest extends AndroidTestCase{
+public class RequestTaskTest{
 	
 	private final String lethargica = "t308758"; //lethargica
 	private final RequestMethod GET = GeaServerHandler.RequestMethod.GET;
 	private final RequestMethod POST = GeaServerHandler.RequestMethod.POST;
+	private final String baseURL = GeaServerHandler.NET_BASE_URL + GeaServerHandler.BASE_RATE_QUERY;
 	private JSONArray response;
+	private JSONObject jobject;
 	private final BasicNameValuePair from = new BasicNameValuePair("from", "rdio");
 	private final BasicNameValuePair id = new BasicNameValuePair("id", lethargica);
-	private final BasicNameValuePair verdict = new BasicNameValuePair("verdict", GeaServerHandler.RATE_UP_ARGUMENT);
+	private final BasicNameValuePair like = new BasicNameValuePair("verdict", GeaServerHandler.RATE_UP_ARGUMENT);
+	private final BasicNameValuePair dislike = new BasicNameValuePair("verdict", GeaServerHandler.RATE_DOWN_ARGUMENT);
 	
 	@Before
 	public void initialize(){
 		response = null;
+		jobject = null;
 	}
 	
 	@Test
 	public void testURL(){
 		String expected = "http://gea.kenpowers.net/rate?from=rdio&id=t308758&verdict=like";
-		String result = GeaServerHandler.getURLStringForParams(GeaServerHandler.NET_BASE_URL + GeaServerHandler.BASE_RATE_QUERY, new BasicNameValuePair[] { from, id, verdict });
+		String result = GeaServerHandler.getURLStringForParams(baseURL, new BasicNameValuePair[] { from, id, like });
 		assertEquals(expected, result);
 	}
 	
-	@Ignore
 	@Test
-	public void testGET(){
-		String baseURL = GeaServerHandler.NET_BASE_URL + GeaServerHandler.BASE_RATE_QUERY;
+	public void testGET() throws JSONException{
 		BasicNameValuePair limit = new BasicNameValuePair("limit", "10");
 		String getURL = GeaServerHandler.getURLStringForParams(baseURL, new BasicNameValuePair[] { limit });
 		response = GeaServerHandler.getJSONForRequest(getURL, GET);
-		JSONObject jobject = null;
-		try{
-			jobject = response.getJSONObject(0);
-			assertEquals(10, response.length()); //make sure we get back 
-			
-		}
-		catch(JSONException e){
-			fail(e.toString());
-		}
+		assertEquals(10, response.length());
+		jobject = response.getJSONObject(0);
+		assertNotNull(jobject.getString("artist"));
+		assertFalse(jobject.getString("artist").equals(""));
+		assertNotNull(jobject.getString("album"));
+		assertFalse(jobject.getString("album").equals(""));
+		assertNotNull(jobject.getString("title"));
+		assertFalse(jobject.getString("title").equals(""));
+		assertNotNull(jobject.getString("id"));
 	}
 	
-	@Ignore
 	@Test
-	public void testPOST(){
-		String postURL = GeaServerHandler.NET_BASE_URL + GeaServerHandler.BASE_RATE_QUERY + "from=rdio&" + "id=" + lethargica + "&verdict=" + GeaServerHandler.RATE_UP_ARGUMENT;
+	public void testPOST() throws JSONException{
+		//String postURL = GeaServerHandler.NET_BASE_URL + GeaServerHandler.BASE_RATE_QUERY + "from=rdio&" + "id=" + lethargica + "&verdict=" + GeaServerHandler.RATE_UP_ARGUMENT;
+		String postURL = GeaServerHandler.getURLStringForParams(baseURL, new BasicNameValuePair[] { from, id, like });
 		response = GeaServerHandler.getJSONForRequest(postURL, POST);
-		JSONObject jobject = null;
-		try{
-			jobject = response.getJSONObject(0);
-			
-		}
-		catch(JSONException e){
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		assertEquals(1, response.length());
+		jobject = response.getJSONObject(0);
+		int rating = jobject.getInt("id");
+		postURL = GeaServerHandler.getURLStringForParams(postURL, new BasicNameValuePair[] { from, id, dislike });
+		response = GeaServerHandler.getJSONForRequest(postURL, POST);
+		jobject = response.getJSONObject(0);
+		int updatedRating = jobject.getInt("id");
+		assertEquals(rating + 1, updatedRating);
+		postURL = GeaServerHandler.getURLStringForParams(postURL, new BasicNameValuePair[] { from, id, like });
+		response = GeaServerHandler.getJSONForRequest(postURL, POST);
+		jobject = response.getJSONObject(0);
+		updatedRating = jobject.getInt("id");
+		assertEquals(rating, updatedRating);
 	}
-	
-	//@Test
-	//public void test
 }
