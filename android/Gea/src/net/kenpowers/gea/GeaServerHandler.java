6c342546f9599c468821c6a2cd35b05f77bc7e2a
George Daole-Wellman <gdaolewellman@gmail.com>
2013-05-01 03:46:10 -0400

    Build settings and instructions

--- android/Gea/src/net/kenpowers/gea/GeaServerHandler.java
+++ android/Gea/src/net/kenpowers/gea/GeaServerHandler.java
@@ -20,8 +20,10 @@ public class GeaServerHandler {
 	
 	private GeaServerHandler() {}
 	
-	public static final String LOCALHOST_BASE_URL = "http://10.0.2.2:3000";
-	public static final String NET_BASE_URL = "http://gea.kenpowers.net";
+	public static final String LOCALHOST_BASE_URL = 
+			MainActivity_.getAppContext().getString(R.string.localhost_base_url);
+	public static final String NET_BASE_URL = 
+			MainActivity_.getAppContext().getString(R.string.net_base_url);
 	public static final String BASE_RATE_QUERY = "/rate?";
 	public static final String RATE_UP_ARGUMENT = "like";
 	public static final String RATE_DOWN_ARGUMENT = "dislike";
